d3d3738cbfe627ea00e0936205ea0a9be1584bb1
Patricio Saldivar <patricio.saldivarf@gmail.com>
2020-09-25 00:01:51 -0500

    Completed Lab 5

--- js/script.js
+++ js/script.js
@@ -1,40 +1,25 @@
-let post = document.getElementById("ButtonPost");
-let clear = document.getElementById("ButtonClear");
-let mark = document.getElementById("ButtonMark");
-let del = document.getElementById("ButtonDelete");
- 
-post.addEventListener("click",TodoPost);
-clear.addEventListener("click",TodoClear);
-mark.addEventListener("click",TodoMark);
-del.addEventListener("click",TodoDel);
+$("#ButtonPost").on("click", (e) => {
+   e.preventDefault();
+   var todo = $("#todoText").val();
+   $("#todoList").append(`<p> <input type = "checkbox" name="todo" id = "todo"/>  ${todo}  <br> </p>`);
+   $("#todoText").val('');
+});
 
+$("#ButtonClear").on("click", (e) => {
+   e.preventDefault();
+   $("#todoList #todo").each(function () {
+      $(this).prop("checked", false);
+   });
+});
 
- 
-function TodoPost(e) {
-e.preventDefault();
- 
-var todo = document.getElementById("todoText").value;
-var list = document.getElementById("todoList");
- 
-let currentList = list.innerHTML;
-list.innerHTML = currentList + '<input type = "checkbox" name="todo" id = "todo"/>  ${todo}  <br>'
-}
- 
-function TodoClear(e) {
-e.preventDefault();
-var list = document.getElementById("todoList");
-list.querySelectorAll("#todo").forEach((item) => item.checked = false);
-   
-}
- 
-function TodoMark(e) {
-e.preventDefault();
-var list = document.getElementById("todoList");
-list.querySelectorAll("#todo").forEach((item) => item.checked = true);
-}
- 
-function TodoDel(e) {
-e.preventDefault();
-var list = document.getElementById("todoList");
-   list.innerHTML = "";
-}
+$("#ButtonMark").on("click", (e) => {
+   e.preventDefault();
+   $("#todoList #todo").each(function () {
+      $(this).prop("checked", true);
+    });
+});
+
+$("#ButtonDelete").on("click", (e) => {
+   e.preventDefault();
+   $("#todoList").html('');
+});
